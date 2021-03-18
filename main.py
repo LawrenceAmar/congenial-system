f528c5306a1e255590698bb937ce9a2c77dfa3fe
MisamoXD <80806278+MisamoXD@users.noreply.github.com>
2021-03-18 20:19:11 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+from kivy.uix.camera import Camera
+from kivy.uix.filechooser import FileChooserListView
+import time
